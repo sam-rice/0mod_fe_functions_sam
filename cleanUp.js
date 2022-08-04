45713fbf064c7bd4c1142709fcd46228dc5e191a
Sam Rice <samsrice@gmail.com>
2022-08-04 12:42:00 -0700

    Initial commit

--- cleanUp.js
+++ cleanUp.js
@@ -3,44 +3,61 @@
 // YOUR TASK:
   // Modify the code to make that improvement(s)
   // write a JS comment to explain what you changed, and why
-  // make sure the code you submit WORKS - you can run this entire file 
+  // make sure the code you submit WORKS - you can run this entire file
     // using `node cleanUp.js` or copy and paste into a replit
 
 
 // EX 1:
 function nameQuestion() {
-  console.log("Hello, what is your name?")
+  console.log("Hello, what is your name?");
 }
 
-nameQuestion()
+nameQuestion();
 
+// Line 12: Added semicolon at end of line for good practice.
+// Line 15: Added semicolon after function call for good practice.
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-console.log(sum);
-      }
+  var sum = first + second + third;
+  console.log(sum);
+}
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
 
+// Line 22: Tab'd line forward for better readability. Added semicolon to end of line for good practice.
+// Line 23: Tab'd line forward for better readability. Added semicolon to end of line for good practice.
+// Line 24: Deleted tabs to bring curly bracket in-line with line 21 for better readability.
+
 
 // EX 3:
-func makeFreshPesto(){
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");
+}
 
 makeFreshPesto();
-
+// Line 35: Corrected <func> to <function>.
+// Line 35: Added space between ending parentheses and opening curly bracket for better readability.
+// Line 41: Moved closing curly bracket from line 40 to 41 for better readability.
 
 //  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
+function average(num1, num2) {
+  var sum = num1 + num2;
+  var avg = sum / 2;
 
-  console.log(`the average is: ${avg}`.)
-  }
+  console.log(`The average is: ${avg}.`);
+}
+
+average(4, 8);
+
+// Line 49: Moved opening curly bracket from line 50 to 49 for better readability.
+// Line 50: Tab'd line forwards for better readability.
+// Line 51: Tab'd line backwards to align with line 50.
+// Line 53: Capitalized first character in string and moved string's punctuation inside of closing backtick.
+// Line 54: Tab'd closing curly bracket backwards for better readability.
+// Line 56: Added function call with arguments.
--- functions.js
+++ functions.js
@@ -1,13 +1,44 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
 
+function printGreeting() {
+  console.log("Welcome in!");
+}
+
+printGreeting();
+printGreeting();
+printGreeting();
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
 
+function mySentence(band) {
+  console.log(`My favorite band is ${band}`);
+}
+
+mySentence("The Beatles");
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
 
+function companyPay(name, low, high) {
+  var range = high - low;
+  console.log(`${name} employees earn salaries within a $${range} range.`);
+}
+
+companyPay("Google", 65000, 800000);
+
 
 // 4: Write a function that satifies the following interaction pattern:
+
+function checkStock(num, item) {
+  if (num >= 4) {
+    console.log(`${item} is stocked`);
+  } else if (num <= 3 && num >= 1) {
+      console.log(`${item} - running LOW`);
+  } else {
+      console.log(`${item} - OUT of stock!`);
+  }
+}
+
+
 
 checkStock(4, "Coffee");
 // => "Coffee is stocked"
